807076d1f6532ffa9ab5384bc965e4789ed5427c
Sarang Holey <sarangholey@gmail.com>
2022-09-24 11:08:37 +0530

    Update _1_Wrapper_Classes.java
    Multiple Ways to convert String amount to integer

--- src/test/java/_5_Session/_3_Data_Conversion/_1_Wrapper_Classes.java
+++ src/test/java/_5_Session/_3_Data_Conversion/_1_Wrapper_Classes.java
@@ -34,6 +34,26 @@ public class _1_Wrapper_Classes {
 //		String g = "$156";
 //		int h = Integer.parseInt(g); // it will throw NumberFormatException
 //		System.out.println(h);
+		
+		// Remove foreign characters while conversion String to integer
+		// Approach 1 - ex amount = "$456"
+		String amount1 = "$456";
+		String temp1 = amount1.replace("$", "");
+		int planeAmount1 = Integer.parseInt(temp1);
+		System.out.println(planeAmount1 + 20);
+		
+		// Approach 2 - ex amount = "$456/-"
+		String amount2 = "$456/-";
+		String temp2 = amount2.substring(1,4);
+		System.out.println("temp 2 " + temp2);
+		int planeAmount2 = Integer.parseInt(temp2);
+		System.out.println(planeAmount2 + 20);
+		
+		// Approach 3 - ex amount = "$4,567,75/-"
+		String amount3 = "$4,567,75/-";
+		String temp3 = amount3.replaceAll("[^0-9]", "");
+		int planeAmount3 = Integer.parseInt(temp3);
+		System.out.println(planeAmount3 + 20);
 	}
 
 }
